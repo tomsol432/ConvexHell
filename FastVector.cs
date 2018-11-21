70747f759a533f3db6df0786ac9076795355c887
tomsol432 <39550133+tomsol432@users.noreply.github.com>
2018-11-21 11:41:00 +0100

    Added Solve()

---
+++ FastVector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MIConvexHull;
+
+namespace ConvexHell
+{
+    class FastVector : IVertex
+    {
+public FastVector(double x, double y)
+        {
+            this.position = new double[2] { x, y };
+        }
+
+        private double[] position;
+public double[] Position { get => position; set => position = value; }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MIConvexHull;
 
 namespace ConvexHell
 {
@@ -43,6 +44,8 @@ namespace ConvexHell
             order = datasetFactory.GenerateOrder();
             DrawPoints(points);
             DrawCenters(points);
+            Solve(center, points, order);
+          
 
         }
 
@@ -71,7 +74,7 @@ namespace ConvexHell
                 Fill = new SolidColorBrush(Color.FromArgb(255, 0, 255, 0)),
                 Stroke = null
             };
-            ellipse.Margin = new Thickness(CanvasMap.Width/2, CanvasMap.Height/2, 0, 0);
+            ellipse.Margin = new Thickness(CanvasMap.ActualWidth/2, CanvasMap.ActualHeight/2, 0, 0);
             CanvasMap.Children.Add(ellipse);
 
             double centerX = 0;
@@ -92,19 +95,83 @@ namespace ConvexHell
                 Fill = new SolidColorBrush(Color.FromArgb(255, 0, 0, 255)),
                 Stroke = null
             };
+            center = new Vector(centerX, centerY);
             ellipse.Margin = new Thickness(centerX, centerY, 0, 0);
             CanvasMap.Children.Add(ellipseCenter);
 
         }
-
+        
         private void ButtonDrawCenter_Click_1(object sender, RoutedEventArgs e)
         {
 
         }
-
-        public void Solve(Vector[] vectors, int [] order)
+       
+        public void Solve(Vector center,Vector[] vectors, int [] order)
         {
+            var verticles = new FastVector[vectors.Length];
+            for (int i = 0; i < verticles.Length; i++)
+            {
+                verticles[i] = new FastVector(vectors[i].X, vectors[i].Y);
+            }
+
+            var convex = ConvexHull.Create(verticles).Points.ToArray();
+
+            DrawHull(convex);
+
 
         }
+        void DrawHull(FastVector[] convexData)
+        {
+            for (int i = 0; i < convexData.Length; i++)
+            {
+                if (i < convexData.Length - 1)
+                {
+                    Line line = new Line()
+                    {
+                        X1 = convexData[i].Position[0],
+                        Y1 = convexData[i].Position[1],
+
+                        X2 = convexData[i + 1].Position[0],
+                        Y2 = convexData[i + 1].Position[1],
+                        StrokeThickness = 2,
+                        Stroke = new SolidColorBrush(Color.FromArgb(255, 255, 255, 255)),
+                    };
+                    CanvasMap.Children.Add(line);
+                }
+
+                if (i == convexData.Length - 1)
+                {
+                    Line line = new Line()
+                    {
+
+                        X1 = convexData[i].Position[0],
+                        Y1 = convexData[i].Position[1],
+
+                        X2 = convexData[0].Position[0],
+                        Y2 = convexData[0].Position[1],
+
+                        StrokeThickness = 2,
+                        Stroke = new SolidColorBrush(Color.FromArgb(255, 255, 255, 255)),
+                    };
+
+                    CanvasMap.Children.Add(line);
+                }
+            }
+        }
+
+
+               
+            
+       
+
+        private static double GetDistance(Vector A, Vector B)
+        {
+            double a = (double)(B.X - A.X);
+            double b = (double)(B.Y - A.Y);
+
+            return Math.Sqrt(a * a + b * b);
+        }
+
+       
     }
 }
